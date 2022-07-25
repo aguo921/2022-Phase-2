5321f8a4748689cb40de5b477dc00aef17ef6f2a
Angela Guo <aguo921@aucklanduni.ac.nz>
2022-07-25 23:10:49 +1200

    Add style

--- 1. Frontend/submission/src/BookList.tsx
+++ 1. Frontend/submission/src/BookList.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import internal from "stream";
+import Rating from "@mui/material/Rating";
+import Divider from "@mui/material/Divider";
+import Tooltip from "@mui/material/Tooltip";
 
 function BookList(props) {
     const bookListItems = props.books.map((book, index: number) => {
         return (
-            <div key={index}>
+            <div key={index} className="book-item">
                 <h2>
                     <a href={book.volumeInfo.previewLink}>
                         {book.volumeInfo.title}
@@ -14,7 +17,19 @@ function BookList(props) {
                 <h3>
                     Author: {book.volumeInfo.authors.join(', ')}
                 </h3>
+                <Divider variant="middle" />
                 <img src={book.volumeInfo.imageLinks.smallThumbnail}/>
+                <Tooltip title={
+                    book.volumeInfo.ratingsCount !== 1 ? (
+                        `${book.volumeInfo.ratingsCount} ratings`
+                    ) : (
+                        `1 rating`
+                    )}
+                    placement="right"
+                >
+                    <span><Rating value={book.volumeInfo.averageRating} readOnly /></span>
+                </Tooltip>
+                {/* TODO: Add accordion to description*/}
                 <p>
                     {book.volumeInfo.description}
                 </p>
